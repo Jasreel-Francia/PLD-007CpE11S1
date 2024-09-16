e2efd35acce734471ad27b868481bdbca0c5f181
Jasreel-Francia <qjcdfrancia@tip.edu.ph>
2024-09-16 12:15:57 +0800

    Add files via upload

---
+++ Challenge/Challenge #2.cpp
@@ -0,0 +1,19 @@
+ #include <iostream>
+ using namespace std; 
+ 
+ int main ()
+ 
+ {
+ 	
+ //Challenge #2 
+	 int i=4;
+ 		do 
+ 	{
+ 		cout<<"Hello"<<i<<endl;
+ 		i++;
+	 }
+ 	while (i<10 && i>3);
+ 	
+return 0;
+ 		
+ }
