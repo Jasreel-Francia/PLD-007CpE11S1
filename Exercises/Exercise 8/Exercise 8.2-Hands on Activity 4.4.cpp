1aa70a541bd718e6b6763a9dc4197efd9bdc52aa
Jasreel-Francia <qjcdfrancia@tip.edu.ph>
2024-11-02 02:18:14 +0800

    Add files via upload

--- Exercises/Exercise 8/Exercise 8.2-Hands on Activity 4.4.cpp
+++ Exercises/Exercise 8/Exercise 8.2-Hands on Activity 4.4.cpp
@@ -1,31 +1,5 @@
 #include <stdio.h>
 #include <ctype.h>
-//int main ()
-//{
-//	printf("%s\n%s%s\n%s%s\n%s%s\n%s%s\n\n", "According to islower:", //Determines if ch is a lowercase letter or no.
-//
-//        islower('p') ? "p is a ": "p is not a ", "lowercase letter",
-//        islower('P') ? "P is a ": "P is not a ", "lowercase letter",
-//        islower('5') ? "5 is a ": "5 is not a ", "lowercase letter",
-//        islower('!') ? "! is a ": "! is not a ", "lowercase letter");
-//        
-//    printf("%s\n%s%s\n%s%s\n%s%s\n%s%s\n\n", "According to isupper:", //Determines if ch is an uppercase letter or no.
-//        isupper('D') ? "D is an ": "D is not an ", "uppercase letter",
-//        isupper('d') ? "d is an ": "d is not an ", "uppercase letter",
-//        isupper('8') ? "8 is an ": "8 is not an ", "uppercase letter",
-//        isupper('&') ? "& is an ": "& is not an ", "uppercase letter");
-//        
-//char ch1=toupper('u'), ch2=toupper('7'), ch3=toupper('$'), ch4=tolower('L'); //Converts ch to its said function.
-//
-//     	printf("Converting: \n");
-//     	printf("u converted to uppercase is %c\n", ch1);
-//		printf("7 converted to uppercase is %c\n", ch2);
-//		printf("$ converted to uppercase is %c\n", ch3);
-//		printf("L converted to lowercase is %c\n", ch4);
-//        
-//        return 0;
-//}
-
 int main ()
 {
 	char ch;
@@ -54,19 +28,17 @@ int main ()
 		 	printf("%c is not an uppercase alphabet. \n", ch);
 		 }
 		 
- 		char upper=tolower(ch), lower=toupper(ch);
+ 			char lowwer=tolower(ch), uppper=toupper(ch);
+ 		
  		if (islower(ch)) //covert to uppercase
  		{
-		 	printf("%c converted to lowercase alphabet is", ch);
-			printf(" %c. \n", upper);
-		 } else {
-		 	printf("%c is already in uppercase. \n", ch);
+		 	printf("%c converted to uppercase alphabet is", ch);
+			printf(" %c. \n", uppper);
+		 } else if (isupper(ch)){
+		 	printf("%c converted to lower alphabet is", ch);
+			printf(" %c. \n", lowwer);
 		 }
-		
-		
-//		 	printf("%c converted to lowercase alphabet is %c. \n", ch, ch1);
-//			printf("%c converted to uppercase alphabet is %c. \n", ch, ch2);
-		
+				
 		if (isdigit(ch)) //digit
  		{
 		 	printf("%c is a digit. \n", ch);
@@ -104,9 +76,30 @@ int main ()
 		
 		if (isgraph(ch)) //Identifies if ch is printing character other than a space or no
  		{
-		 	printf("%c is a digit. \n", ch);
+		 	printf("%c is a printing character (graph). \n", ch);
 		 } else {
-		 	printf("%c is not a digit. \n", ch);
+		 	printf("%c is not a printing character (graph). \n", ch);
+		 }
+		
+		if (iscntrl(ch)) //control
+ 		{
+		 	printf("%c is a control character. \n", ch);
+		 } else {
+		 	printf("%c is not a control character. \n", ch);
+		 }
+		
+		if (isprint(ch)) //printing character (character)
+ 		{
+		 	printf("%c is a printing character. \n", ch);
+		 } else {
+		 	printf("%c is not a printing character. \n", ch);
+		 }
+		
+		if (ispunct(ch)) //printing character (digits)
+ 		{
+		 	printf("%c is a printing character(digits). \n", ch);
+		 } else {
+		 	printf("%c is not a printing character(digits). \n", ch);
 		 }
 		
 		
